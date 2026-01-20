0914b3d14b55f79ad23d854502eff22c262313b2
Bruno-Camargo-V3ktor <bruno.ferreira.v3ktor@gmail.com>
2026-01-20 14:40:06 -0300

    feat/test: impl Open file from bytes and subs structs, and save file, create test for duplicate file

--- src/audios/wav.rs
+++ src/audios/wav.rs
@@ -1,3 +1,10 @@
+use std::{
+    fs::File,
+    io::{Cursor, Read, Write},
+};
+
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+
 use super::Audio;
 
 #[derive(Clone, Debug)]
@@ -29,9 +36,107 @@ struct FileHeader {
     data_size: u32,
 }
 
+impl FileHeader {
+    pub fn from_bytes(bytes: &[u8]) -> Self {
+        let mut cursor = Cursor::new(bytes);
+
+        Self {
+            //
+            type_bloc_id: [
+                cursor.read_u8().unwrap() as char,
+                cursor.read_u8().unwrap() as char,
+                cursor.read_u8().unwrap() as char,
+                cursor.read_u8().unwrap() as char,
+            ],
+            file_size: cursor.read_u32::<LittleEndian>().unwrap(),
+            format_id: [
+                cursor.read_u8().unwrap() as char,
+                cursor.read_u8().unwrap() as char,
+                cursor.read_u8().unwrap() as char,
+                cursor.read_u8().unwrap() as char,
+            ],
+
+            //
+            format_bloc_id: [
+                cursor.read_u8().unwrap() as char,
+                cursor.read_u8().unwrap() as char,
+                cursor.read_u8().unwrap() as char,
+                cursor.read_u8().unwrap() as char,
+            ],
+            bloc_size: cursor.read_u32::<LittleEndian>().unwrap(),
+            audio_format: cursor.read_u16::<LittleEndian>().unwrap(),
+            nbr_channels: cursor.read_u16::<LittleEndian>().unwrap(),
+            frequency: cursor.read_u32::<LittleEndian>().unwrap(),
+            byte_per_sec: cursor.read_u32::<LittleEndian>().unwrap(),
+            byte_per_bloc: cursor.read_u16::<LittleEndian>().unwrap(),
+            bits_per_sample: cursor.read_u16::<LittleEndian>().unwrap(),
+
+            //
+            data_bloc_id: [
+                cursor.read_u8().unwrap() as char,
+                cursor.read_u8().unwrap() as char,
+                cursor.read_u8().unwrap() as char,
+                cursor.read_u8().unwrap() as char,
+            ],
+            data_size: cursor.read_u32::<LittleEndian>().unwrap(),
+        }
+    }
+
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = Vec::with_capacity(44);
+
+        let _ = bytes.write_u8(self.type_bloc_id[0] as u8);
+        let _ = bytes.write_u8(self.type_bloc_id[1] as u8);
+        let _ = bytes.write_u8(self.type_bloc_id[2] as u8);
+        let _ = bytes.write_u8(self.type_bloc_id[3] as u8);
+
+        let _ = bytes.write_u32::<LittleEndian>(self.file_size);
+
+        let _ = bytes.write_u8(self.format_id[0] as u8);
+        let _ = bytes.write_u8(self.format_id[1] as u8);
+        let _ = bytes.write_u8(self.format_id[2] as u8);
+        let _ = bytes.write_u8(self.format_id[3] as u8);
+
+        //
+        let _ = bytes.write_u8(self.format_bloc_id[0] as u8);
+        let _ = bytes.write_u8(self.format_bloc_id[1] as u8);
+        let _ = bytes.write_u8(self.format_bloc_id[2] as u8);
+        let _ = bytes.write_u8(self.format_bloc_id[3] as u8);
+
+        let _ = bytes.write_u32::<LittleEndian>(self.bloc_size);
+        let _ = bytes.write_u16::<LittleEndian>(self.audio_format);
+        let _ = bytes.write_u16::<LittleEndian>(self.nbr_channels);
+        let _ = bytes.write_u32::<LittleEndian>(self.frequency);
+        let _ = bytes.write_u32::<LittleEndian>(self.byte_per_sec);
+        let _ = bytes.write_u16::<LittleEndian>(self.byte_per_bloc);
+        let _ = bytes.write_u16::<LittleEndian>(self.bits_per_sample);
+
+        //
+        let _ = bytes.write_u8(self.data_bloc_id[0] as u8);
+        let _ = bytes.write_u8(self.data_bloc_id[1] as u8);
+        let _ = bytes.write_u8(self.data_bloc_id[2] as u8);
+        let _ = bytes.write_u8(self.data_bloc_id[3] as u8);
+        let _ = bytes.write_u32::<LittleEndian>(self.data_size);
+
+        bytes
+    }
+}
+
 #[derive(Clone, Debug)]
 struct Payload {
     data: Vec<u8>,
+}
+
+impl Payload {
+    pub fn from_bytes(bytes: &[u8]) -> Self {
+        Self {
+            data: bytes.to_vec(),
+        }
+    }
+
+    pub fn to_bytes(&self) -> Vec<u8> {
+        self.data.clone()
+    }
 }
 
 // Impls
@@ -40,11 +145,32 @@ impl Audio for Wav {
     where
         Self: Sized,
     {
-        todo!()
+        let mut file = File::open(path.into())?;
+        let mut bytes = Vec::with_capacity(100);
+
+        let _size = file.read_to_end(&mut bytes)?;
+
+        let header = FileHeader::from_bytes(&bytes[0..44]);
+        let payload = Payload::from_bytes(&bytes[44..]);
+
+        Ok(Self { header, payload })
     }
 
     fn save(&mut self, path: impl Into<String>, overwrite: bool) -> std::io::Result<()> {
-        todo!()
+        let mut all_bytes = Vec::new();
+
+        all_bytes.append(&mut self.header.to_bytes());
+        all_bytes.append(&mut self.payload.to_bytes());
+
+        if overwrite {
+            let mut file = File::create(path.into())?;
+            file.write(&all_bytes)?;
+            return Ok(());
+        }
+
+        let mut file = File::create_new(path.into())?;
+        file.write(&all_bytes)?;
+        Ok(())
     }
 
     fn sample_rate(&self) -> u32 {
@@ -57,5 +183,17 @@ impl Audio for Wav {
 
     fn bit_depth(&self) -> u8 {
         todo!()
+    }
+}
+
+// tests
+#[cfg(test)]
+mod tests {
+    use crate::{Audio, Wav};
+
+    #[test]
+    pub fn test_duplicate_file() {
+        let mut audio = Wav::open("./audios/suzume_no_tojimari.wav").unwrap();
+        audio.save("./audios/duplicate.wav", false).unwrap();
     }
 }
