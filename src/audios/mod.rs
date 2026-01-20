28dbbd2243236de98b224d72e6e22c51864596d4
Bruno-Camargo-V3ktor <bruno.ferreira.v3ktor@gmail.com>
2026-01-19 23:42:14 -0300

    feat: defined basic trait for represent of audio

---
+++ src/audios/mod.rs
@@ -0,0 +1,15 @@
+use std::io::Result as IOResult;
+
+pub trait Audio {
+    fn open(path: impl Into<String>) -> IOResult<Self>
+    where
+        Self: Sized;
+
+    fn save(&mut self, path: impl Into<String>, overwrite: bool) -> IOResult<()>;
+
+    fn sample_rate(&self) -> u32;
+
+    fn bit_depth(&self) -> u8;
+
+    fn channels(&self) -> u8;
+}
